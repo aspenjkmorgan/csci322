798741da34c39ec72ca65824014a7f2b3c86f7c1
hotcasper <122695959+hotcasper@users.noreply.github.com>
2024-02-09 09:06:22 -0700

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,39 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const {randomUUID} = require("crypto");
+const admin = require('firebase-admin');
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.flashbriefing = onRequest(
+    {timeoutSeconds: 15, cors: true, maxInstances: 10},
+    (request, response) => {
+      logger.info("Flash Briefing Requested!", {structuredData: true});
+      response.set("Content-Type", "application/json").json(
+          [
+            {
+              "uid": randomUUID(),
+              "updateDate": new Date().toISOString(),
+              "titleText": "Example test",
+              "mainText": "example answer",
+              "streamUrl": null,
+              "redirectionUrl": null,
+            },
+          ],
+      );
+    });
